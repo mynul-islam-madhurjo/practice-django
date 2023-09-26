2b451805d4ce05baaeb4b4ce1f9b04b8704f832b
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-09-26 09:36:09 +0600

    Changing the model

--- trydjango/products/models.py
+++ trydjango/products/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=120)  # max length required
     description = models.TextField(blank=True, null=True)
-    price = models.DecimalField(decimal_places=2,max_digits=10000)
-
-    # New
+    price = models.DecimalField(decimal_places=2, max_digits=10000)
     summary = models.TextField()
+
+    # Adding new field
+    features = models.BooleanField(null=True)  # default='Empty'
