96a01b1a6303f6aef1a494b2001bddd3ca9cdda0
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-09-26 12:07:28 +0600

    Django Templates

--- trydjango/mypages/views.py
+++ trydjango/mypages/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello_world(request, *args,**kwargs):
-    #print(request.user)
-    return HttpResponse("<html><body><h1>Hello, World!</h1></body></html>")
+    # print(request.user)
+    # return HttpResponse("<html><body><h1>Hello, World!</h1></body></html>")
+    return render(request,'home.html',{})
