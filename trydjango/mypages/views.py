d72d03f4f39a4861bc2052e688f9dabb5f1685f5
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-09-26 11:47:21 +0600

    requesting user's name

--- trydjango/mypages/views.py
+++ trydjango/mypages/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hello_world(request):
+def hello_world(request, *args,**kwargs):
+    #print(request.user)
     return HttpResponse("<html><body><h1>Hello, World!</h1></body></html>")
