7e812f3d14c4f96faa355598b29919e3d31c201f
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-09-29 02:07:16 +0600

    Form Widgets

--- trydjango/products/forms.py
+++ trydjango/products/forms.py
@@ -14,6 +14,19 @@ class ProductForm(forms.ModelForm):
 
 
 class RawProductForm(forms.Form):
-    title = forms.CharField()
-    description = forms.CharField()
-    price = forms.DecimalField()
+    title = forms.CharField(label='',
+                            widget=forms.TextInput(
+                                attrs={
+                                   'placeholder':'Type here the title'
+                                }
+                            ))
+    description = forms.CharField(required=True,
+                                  widget=forms.Textarea(
+                                      attrs={
+                                          'placeholder': 'Description',
+                                          'class': 'new-class-name two',
+                                          'rows': 20,
+                                          'cols': 50
+                                      }
+                                  ))
+    price = forms.DecimalField(initial=199.99)
