2e780b056faca9a2384e52242116562ed6544b27
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-09-29 00:41:57 +0600

    Raw HTML form

--- trydjango/products/views.py
+++ trydjango/products/views.py
@@ -6,24 +6,28 @@ from .forms import ProductForm
 
 
 # Create your views here.
-
-
 def create_product(request):
-
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        #clear the form
-        form = ProductForm
-
+    if request.method == "POST":
+        my_title = request.POST.get('title')
+        print(my_title)
     context = {
-        'form' : form
     }
-    return render(request,'create.html', context)
+    return render(request, 'create.html', context)
+
+# def create_product(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         # clear the form
+#         form = ProductForm
+#
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'create.html', context)
 
 
 def show_product(request):
-
     obj = Product.objects.get(id=1)
     context = {'product': obj}
-    return render(request,'products/product_view.html', context)
+    return render(request, 'products/product_view.html', context)
