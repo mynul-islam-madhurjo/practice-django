fedfc81c8e913820e1dfcb6a17d296279a7b4a65
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-10-10 10:34:12 +0600

    Creating first API

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,9 @@
 import requests
 
-endpoint = 'http://localhost:8000/'
+endpoint = 'http://localhost:8000/api'
 
 get_response = requests.get(endpoint)
 
-print(get_response.text)
-print(get_response.status_code)
+# print(get_response.text)
+# print(get_response.status_code)
+print(get_response.json())
