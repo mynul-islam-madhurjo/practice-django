ece76d0e251f0f778be81aea3b360a1e79daefaf
mynul-islam-madhurjo <iammynul.bracu@gmail.com>
2023-10-12 10:05:39 +0600

    Rest framework model serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -2,11 +2,19 @@ from rest_framework import serializers
 from .models import Product
 
 
+# We can have multiple serializers of the same model it can be used for data cleaning as well
+
 class ProductSerializer(serializers.ModelSerializer):
+    my_discount = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Product
         fields = [
             'title',
             'price',
-            'sale_price'
+            'sale_price',
+            'my_discount',
         ]
+
+    def get_my_discount(self, obj):
+        return obj.get_discount()
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,17 +2,17 @@ import requests
 
 endpoint = 'http://localhost:8000/products'
 
-# get_response = requests.get(endpoint)
+get_response = requests.get(endpoint)
 # get_response = requests.get(endpoint, params={"abc": 123}, json={'query': 'Hello'})
 
 # print(get_response.text)
 # print(get_response.status_code)
-# print(get_response.json())
+print(get_response.json())
 
 
 # Send the parameters in the query string
-params = {"abc": 123, "query": "Hello"}
-get_response = requests.get(endpoint, params=params)
-
-print(get_response.text)
-print(get_response.status_code)
+# params = {"abc": 123, "query": "Hello"}
+# get_response = requests.get(endpoint, params=params)
+#
+# print(get_response.text)
+# print(get_response.status_code)
